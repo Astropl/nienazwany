fc53bf0df3d158f219318053bd8c6b80f08d726e
Astropl <martys.pawel@gmail.com>
2020-06-03 16:34:27 +0200

    Add files via upload

--- main2.cpp
+++ main2.cpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <QApplication>
+#include "QFile"
+
 
 using namespace std;
 fstream plikOdczyt;
+QFile plikOdczyt2;
 
 Main2::Main2(QWidget *parent) :
     QDialog(parent),
@@ -27,7 +30,21 @@ void Main2::on_pushButton_clicked()
     QString nrSeryjny1 ;
     QString producent ;
     QString model;
-   plikOdczyt.open("C:/Qt/Pliki/tekst.txt",ios::in);
+
+    //QFile plikOdczyt2 ("C:/Qt/Pliki/tekst.txt");
+    //plikOdczyt2.open("C:/Qt/Pliki/tekst.txt",ios::in);
+plikOdczyt.open("C:/Qt/Pliki/tekst.txt",ios::in);
+
+//    if(plikOdczyt2.open(QFile::Text | QFile::ReadOnly))
+//    {
+//        QTextStream in(&plikOdczyt2);
+//        while (!in.atEnd())
+//        {
+//            std::cout <<plikOdczyt2<<in.readLine();
+//        }
+//        plikOdczyt2.close();
+//    }
+
    if(plikOdczyt.good()==false)
    {
        cout<<"Plik nie istnieje !!!!!";
@@ -39,10 +56,12 @@ void Main2::on_pushButton_clicked()
    {
        switch(nr_lini)
        {
-       case 1: nrSeryjny1;
-           std::cout<<nrSeryjny1.toStdString()<<endl;break;
-       case 2: producent; std::cout<<producent.toStdString()<<endl; break;
-       case 3: model;std::cout<<model.toStdString()<<endl; break;
+       case 1:  linia = nrSeryjny1.toStdString();
+            std::cout<<nrSeryjny1.toStdString()<<endl;break;
+       case 2: linia= producent.toStdString()   ;
+           std::cout<<producent.toStdString()<<endl; break;
+       case 3:  linia =model.toStdString() ;
+           std::cout<<model.toStdString()<<endl; break;
        }
 
 
